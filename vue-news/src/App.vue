f0f78590dd494d7e72df3d826c146ae0ffec1b5f
cre4tive97 <harryborrison5148@gmail.com>
2021-10-24 20:33:41 +0900

    ToolBar.vue with router-link

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,23 +1,18 @@
 <template>
   <div id="app">
+    <ToolBar></ToolBar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import ToolBar from "./components/ToolBar.vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    ToolBar,
+  },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
