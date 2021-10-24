0f0d097a9b4e850dfc5c42e4a569467abd3b1754
cre4tive97 <harryborrison5148@gmail.com>
2021-10-24 21:00:36 +0900

    axios news

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,9 +1,28 @@
 <template>
-  <div>News</div>
+  <div>
+    <div v-for="(user, i) in users" :key="i">{{ user.title }}</div>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  created() {
+    axios
+      .get("https://api.hnpwa.com/v0/news/1.json")
+      .then((response) => {
+        console.log(response.data);
+        this.users = response.data;
+        console.log(this.users);
+      })
+      .catch((error) => console.log(error));
+  },
+};
 </script>
 
 <style></style>
