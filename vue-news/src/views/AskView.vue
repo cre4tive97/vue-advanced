020f3dc4988778c229d8bd53170d5b20ccf8b351
cre4tive97 <harryborrison5148@gmail.com>
2021-10-24 21:18:54 +0900

    axios api modularization

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>Ask</div>
+  <div>
+    <div v-for="(ask, i) in asks" :key="i">{{ ask.title }}</div>
+  </div>
 </template>
 
 <script>
-export default {};
+import { fetchAskList } from "../api";
+export default {
+  name: "AskView",
+  data() {
+    return {
+      asks: [],
+    };
+  },
+  created() {
+    fetchAskList()
+      .then((response) => (this.asks = response.data))
+      .catch((error) => console.log(error));
+  },
+};
 </script>
 
 <style></style>
--- vue-news/src/views/JobsView.vue
+++ vue-news/src/views/JobsView.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>Jobs</div>
+  <div>
+    <div v-for="(job, i) in jobs" :key="i">{{ job.title }}</div>
+  </div>
 </template>
 
 <script>
-export default {};
+import { fetchJobsList } from "../api";
+export default {
+  name: "JobsView",
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  created() {
+    fetchJobsList()
+      .then((response) => (this.jobs = response.data))
+      .catch((error) => console.log(error));
+  },
+};
 </script>
 
 <style></style>
