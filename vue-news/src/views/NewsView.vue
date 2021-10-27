148befc740c8cec11356860f4868037b422c24a7
cre4tive97 <harryborrison5148@gmail.com>
2021-10-27 14:30:16 +0900

    loading spinner event bus

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -6,13 +6,16 @@
 
 <script>
 import ListItem from "../components/ListItem.vue";
+import bus from "../utils/bus.js";
 export default {
   name: "NewsView",
   components: {
     ListItem,
   },
   created() {
+    bus.$emit("start:spinner");
     this.$store.dispatch("FETCH_NEWS");
+    bus.$emit("end:spinner");
   },
 };
 </script>
